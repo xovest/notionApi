c990ab4bb533242526fdd9dc8746683c2d1c7bf5
xovest <79008917+xovest@users.noreply.github.com>
2021-11-13 17:15:40 +0200

    got them linked to the table and it works cool

--- server.js
+++ server.js
@@ -1,13 +1,38 @@
 require('dotenv').config();
 const express = require('express');
-const notion = require('./notion');
+const { getTags, createSuggestion } = require('./notion');
 
 const app = express();
 app.set('views', './views');
 app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true }));
+
+let tags = [];
+getTags().then(data => {
+  tags = data;
+});
+
+setInterval(async () => {
+  tags = await getTags();
+}, 1000 * 60 * 60);
 
 app.get('/', (req, res) => {
-  res.render('index');
+  res.render('index', { tags });
+});
+
+app.post('/create-suggestion', async (req, res) => {
+  const { title, description, isProject, tagIds = [] } = req.body;
+
+  await createSuggestion({
+    title,
+    description,
+    isProject: isProject != null,
+    tags: (Array.isArray(tagIds) ? tagIds : [tagIds]).map(tagId => {
+      return { id: tagId };
+    })
+  });
+
+  res.redirect('/');
 });
 
 app.listen(process.env.PORT)
